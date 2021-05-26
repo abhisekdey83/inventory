e41996dc0faff0d5b39840a897e839218088bcf6
Abhisek <abhisek.consultant@gmail.com>
2021-05-26 11:08:24 +0530

    Initial Comment

---
+++ inventory/settings/development.py
@@ -0,0 +1,29 @@
+from .base import *
+
+ALLOWED_HOSTS = ['127.0.0.1']
+
+DEBUG = True
+
+INSTALLED_APPS += [
+    'debug_toolbar',
+]
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+INTERNAL_IPS = '127.0.0.1'
+
+MIDDLEWARE += ['debug_toolbar.middleware.DebugToolbarMiddleware', ]
+
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
+DEBUG_TOOLBAR_CONFIG = {
+    'JQUERY_URL': '',
+}
